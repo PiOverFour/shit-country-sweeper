3843f8f508d0e0fe46a4bd80a6a39d2bd484594c
Damien Picard <dam.pic@free.fr>
2018-04-23 21:13:17 +0200

    export.sh: autozip

--- export.sh
+++ export.sh
@@ -6,7 +6,10 @@ mkdir -p export/SCS.mac/
 mkdir -p export/SCS.win/
 
 ~/opt/Godot_v3.0.2-stable_x11.64 --export "Linux/X11" export/SCS.x11/SCS.x86_64
-~/opt/Godot_v3.0.2-stable_x11.64 --export "HTML5" export/SCS.HTML/SCS.html
+# ~/opt/Godot_v3.0.2-stable_x11.64 --export "HTML5" export/SCS.HTML/SCS.html
 ~/opt/Godot_v3.0.2-stable_x11.64 --export "Mac OSX" export/SCS.mac/SCS.zip
 ~/opt/Godot_v3.0.2-stable_x11.64 --export "Windows Desktop" export/SCS.win/SCS.exe
 
+zip -r export/SCS.x11.zip export/SCS.x11/*
+zip -r export/SCS.mac.zip export/SCS.mac/*
+zip -r export/SCS.win.zip export/SCS.win/*
